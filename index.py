4860d5176007af6aa2a45f77ae7b55ec0149fbd5
小t <737005431@qq.com>
2017-10-20 22:30:26 +0800

    test

--- index.py
+++ index.py
@@ -1,31 +1,21 @@
-#-*- coding:utf-8 -*-
-
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body_str = """
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Welcome to BAE</title>
-        </head>
-        <body>
-            <h4>Welcome to Baidu App Engine!</h4>
-            <h2>欢迎开启BAE之旅！</h2>
-            <br>
-            <br>
-            <ul>以下链接可能对您有用：</ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/QuickGuide.html" target="_blank">入门指南</a></ul>
-            <ul><a href="http://developer.baidu.com/forum/topic/list?boardId=66" target="_blank">论坛</a></ul>
-            <ul><a href="http://www.baeblog.com/" target="_blank">技术博客</a></ul>
-            <ul><a href="http://weibo.com/baiduappengine" target="_blank">微博</a></ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/FAQ.html" target="_blank">常见问题</a></ul>
-        </body>
-    </html>
-    """
-    body=[body_str]
-    return body
-
+# -*- coding:utf-8 -*-
+from bottle import Bottle, request, view
 from bae.core.wsgi import WSGIApplication
+from netEaseapi import get_url
+
+app = Bottle()
+
+
+@app.route('/music', method=['GET', 'POST'])
+@view('index')
+def decode_music():
+    if request.method == 'GET':
+        text = 'Input Music Url'
+    else:
+        m_url = request.POST.get('music_url')
+        text = get_url(m_url)
+    info = {'text': text}
+    return info
+
+
 application = WSGIApplication(app)
